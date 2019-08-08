adce068a417f0bd66040d3a0595a810e1775031b
Jordan Hall <jordan@hall05.co.uk>
2019-08-08 12:27:08 +0100

    Update version constraint

--- src/test.php
+++ src/test.php
@@ -8,7 +8,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $connection = (new SSHConnection())
     ->to(getenv('TEST_HOSTNAME'))
     ->as(getenv('TEST_USERNAME'))
-    ->withKeyPair('/home/jordan/.ssh/id_rsa.pub', '/home/jordan/.ssh/id_rsa');
+    ->withPrivateKey('/home/jordan/.ssh/id_rsa');
 
 $array = (new Server($connection))
     ->metrics()
